93ee0fcdf2bce5cb7eecf1830c275634a6293fa3
Jorge Jimenez <calenocam24@gmail.com>
2020-06-28 20:05:28 -0400

    added State, SideEffects and their respective hooks

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,31 @@
 // Write your Character component here
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import CharacterCard from "./CharacterCard"
+
+
+const Character = () => {
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        //getting API data from:
+        axios.get(`https://rickandmortyapi.com/api/character`)
+        .then(res => {
+            console.log('Response from useEffect of MainCharacter', res);
+            setData(res.data);
+        },[] )
+        .catch(err => {
+            console.log('Error occured in useEffect of Character', err);
+        });
+    });
+
+    return (
+    
+            <div>
+                <CharacterCard />
+            </div>
+
+    )
+}
+
+export default Character;
---
+++ starwars/src/components/CharacterCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const CharacterCard = props => {
+    console.log("Props from CharacterCard", props );
+
+    return (
+        <div>
+          <h1></h1>
+        </div>
+    )
+}
+
+export default CharacterCard;
